c6de74cc3f8ab629a7ba26e177b294b74d1b9ce2
AKira0169 <odayterk75@gmail.com>
2024-07-26 22:46:00 +0300

    init

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+
+const app = express();
+
+const port = 8000;
+
+const server = app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`App running on port ${port}`);
+});
