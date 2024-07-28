6854bee63564501b8f3d625a82fe1184273118cc
AKira0169 <odayterk75@gmail.com>
2024-07-28 18:37:39 +0300

    authorization

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const userController = require("../controllers/usersController");
 const authController = require("../controllers/authController");
+const authorization = require("../middleware/authorization");
 const app = require("../app");
 
 const router = express.Router();
 
 router.post("/signUp", authController.signUp);
 router.post("/logIn", authController.logIn);
-router.use(authController.protect);
+router.use(authController.protect, authorization("admin"));
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.getUser);
 
