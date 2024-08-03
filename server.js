e01c381f6a792d1d182cb9accd8d246e0e890cec
AKira0169 <odayterk75@gmail.com>
2024-08-03 15:05:36 +0300

    onlinedb

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = require("./app");
 dotenv.config({ path: "./config.env" });
-const DB = process.env.DATABASE;
+
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
 
 const port = process.env.PORT || 7000;
+console.log(DB);
 mongoose.connect(DB).then(() => {
   // eslint-disable-next-line no-console
   console.log("Database connected successfully");
