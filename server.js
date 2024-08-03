ac71b2c5d1c4cdc1779dab2d62ca94587214f0d7
AKira0169 <odayterk75@gmail.com>
2024-08-03 14:47:57 +0300

    onlinedatabase

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 dotenv.config({ path: './config.env' });
-const DB = process.env.DATABASE;
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
 const port = process.env.PORT || 7000;
 mongoose.connect(DB).then(() => {
